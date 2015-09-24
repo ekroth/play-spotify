d788f0f7dbb516ac79004c55ab4a591090ae73b0
Andrée Ekroth <andree.ekroth@gmail.com>
2015-09-24 14:00:05 +0200

    add license header

--- src/main/scala/com/github/ekroth/spotify/Caching.scala
+++ src/main/scala/com/github/ekroth/spotify/Caching.scala
@@ -1,3 +1,9 @@
+/* Copyright (c) 2015 Andrée Ekroth.
+ * Distributed under the MIT License (MIT).
+ * See accompanying file LICENSE or copy at
+ * http://opensource.org/licenses/MIT
+ */
+
 package com.github.ekroth.spotify
 
 trait Caching {
--- src/main/scala/com/github/ekroth/spotify/Objects.scala
+++ src/main/scala/com/github/ekroth/spotify/Objects.scala
@@ -1,3 +1,9 @@
+/* Copyright (c) 2015 Andrée Ekroth.
+ * Distributed under the MIT License (MIT).
+ * See accompanying file LICENSE or copy at
+ * http://opensource.org/licenses/MIT
+ */
+
 package com.github.ekroth.spotify
 
 /** Objects corresponding to Spotify's object model.
